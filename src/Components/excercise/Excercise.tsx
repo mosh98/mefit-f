9c18b864d9ebde6b5f3c5a6e16e914a9d16d57b3
mosh98 <mosleh.edu@gmail.com>
2023-03-08 11:44:53 +0100

    toggle show

--- src/Components/excercise/Excercise.tsx
+++ src/Components/excercise/Excercise.tsx
@@ -1,24 +1,41 @@
 import {useNavigate} from "react-router-dom";
 import ExcerciseData from "./ExcerciseDataView";
 import * as React from "react";
+import {useState} from "react";
+import './ExcerciseStyles.css';
+
+
 
 const Excercise = () => {
     // const navigate = useNavigate();
+    //chest
+    const [showChestAccordion, setShowChestAccordion] = useState(false);
+    //TODO:back
+    const [showBackAccordion, setShowBackAccordion] = useState(false);
+    //TODO:biceps
+    const [showBicepsAccordion, setShowBicepsAccordion] = useState(false);
+    //TODO:triceps
+    const [showTricepsAccordion, setShowTricepsAccordion] = useState(false);
+    //TODO: shoulders
+
+
+
+    const toggleChestAccordion = (e: any) => {
+        e.stopPropagation(); // prevent toggling the main accordion
+        setShowChestAccordion((prevState) => !prevState);
+    };
 
     return (
-        <>
-            <h2>Excercise (Different Body Parts )</h2>
-            <div style={{ display: 'flex' }}>
-                <div style={{ marginRight: 50 }}>
-                    <h6>Chest Workout</h6>
-                    <ExcerciseData muscle="chest" />
-                </div>
-                <div>
-                    <h6>Quadriceps Workout</h6>
-                    <ExcerciseData muscle="quadriceps" />
-                </div>
+        <div>
+            <h1>Excercise</h1>
+            <h4>Choose your own parts to train</h4>
+            <div>
+                <button
+                    className="accordion-button" onClick={toggleChestAccordion}>Chest</button>
+                {showChestAccordion && (<ExcerciseData muscle="chest" />)}
             </div>
-        </>
+
+        </div>
     );
 }
 
