272bf30f5c940e019492363b4926dddd10c7a206
LBohman <lovisabo@hotmail.com>
2023-03-08 11:29:32 +0100

    Remove unused components

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,9 @@
 import './App.css';
-import LoginForm from './components/LoginForm';
-import RegisterForm from './components/RegisterForm';
 
 //Test
 function App() {
   return (
     <div className="App">
-      <LoginForm />
-      {/* <RegisterForm /> */}
     </div>
   );
 }
