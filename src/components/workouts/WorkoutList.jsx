ae77e4edb8b6ee7fd68abef5a7250f7002c35aa5
LBohman <lovisabo@hotmail.com>
2023-03-21 15:00:28 +0100

    Can display all exercises inside a workout in a simple ul-list

--- src/components/workouts/WorkoutList.jsx
+++ src/components/workouts/WorkoutList.jsx
@@ -3,6 +3,7 @@ import {Link} from "react-router-dom";
 function WorkoutList(props) {
 
   const { workouts, error } = props;
+  console.log(workouts);
 
   if (error) {
     return <p>{error}</p>;
@@ -13,16 +14,32 @@ function WorkoutList(props) {
   }
 
 
+  // const allExerciseIds = new Set(
+  //   workouts.flatMap(workout => workout.exercises.map(exercise => exercise.id))
+  // );
+  
+  // console.log(allExerciseIds);
+
+
+
   return (
     <div>
       <ul>
-        {workouts.map((p) => (
+        {/* {workouts.map((p) => (
           <li key={p.id}>
             <Link to={ "/workouts/" + p.id } >
-              <span>{p.name}</span> -
+              <span>{p.name}</span>
               <span>{p.type}</span>
               <span>{p.type}</span>
             </Link>
+          </li>
+        ))} */}
+        {workouts.map((p) => (
+          <li key={p.id}>
+            <p>{p.name}</p>
+            <ul>
+              {p.exercises.map(exercise => <li key={exercise.id}>{exercise.name}</li>)}
+            </ul>
           </li>
         ))}
       </ul>
