ef91f788eaab8178a623b10a468a0ae35a036a9e
Silvia.A.A <silvia.alvarez.allende@se.experis.com>
2023-03-21 16:15:57 +0100

    Add: Create new user for backend using KeycloakRoute.jsx

--- src/routes/KeycloakRoute.jsx
+++ src/routes/KeycloakRoute.jsx
@@ -1,5 +1,7 @@
-import { Navigate } from "react-router-dom";
+import {Navigate} from "react-router-dom";
 import keycloak from "../keycloak";
+import {useEffect, useState} from "react";
+import axios from "axios";
 
 /**
  * Protect routes based on a given role and authenticated status of a Keycloak Session.
@@ -8,19 +10,69 @@ import keycloak from "../keycloak";
  * @returns {JSX.Element}
  */
 
+const apiUrl = process.env.REACT_APP_API_URL;
+
 // Make redirectTo optional
 // If not provided, default to "/"
-function KeycloakRoute({ children, role, redirectTo = "/" }) {
+function KeycloakRoute({children, role, redirectTo = "/"}) {
+    const { sub } = keycloak.tokenParsed;
+ //   const [userCreated, setUserCreated] = useState(false);
 
-      if (!keycloak.authenticated) {
-     return <Navigate replace to={redirectTo} />;
-      }
+    useEffect(() => {
+        const fetchUsers = async () => {
+            try {
+                const response = await axios.get(`${apiUrl}users/userByKeycloakId/${sub}`, {
+                    headers: {
+                        Authorization: `Bearer ${keycloak.token}`,
+                        'Content-Type': 'application/json',
+                    },
+                });
 
-      if (keycloak.hasRealmRole(role)) {
-     return <>{children}</>;
-      }
+                // If user found in database do nothing
+                console.log("User found in database: ", response.data);
+              //  setUserCreated(true);
+                return {user: response.data, error: null};
+            } catch (error) {
+                // If user not found in database, create user
+                console.log("User not found in database: ", error.message);
+                const user = {
+                    e_mail: keycloak.tokenParsed?.email,
+                    first_name: keycloak.tokenParsed?.given_name,
+                    last_name: keycloak.tokenParsed?.family_name,
+                    keyCloakId: keycloak.tokenParsed?.sub,
+                };
 
-      return <Navigate replace to={redirectTo} />;
+
+                    try {
+                        const response = await axios.post(`${apiUrl}users/newUser`, user, {
+                            headers: {
+                                'Authorization': `Bearer ${keycloak.token}`,
+                                'Content-Type': 'application/json',
+                            }
+                        });
+
+                  //      setUserCreated(true);
+                        return {user: response.data, error: null};
+                    } catch (error) {
+                        return {user: [], error: error.message};
+                    }
+
+            }
+        };
+
+        fetchUsers();
+    }, [sub]);
+
+    if (!keycloak.authenticated) {
+        return <Navigate replace to={redirectTo}/>;
+    }
+
+    if (keycloak.hasRealmRole(role)) {
+        return <>{children}</>;
+    }
+
+    return <Navigate replace to={redirectTo}/>;
 
 }
+
 export default KeycloakRoute;
