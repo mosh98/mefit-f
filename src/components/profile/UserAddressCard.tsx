ad6b04a3e7cf8716fcc9a395dda33e03d52d7701
mosh98 <mosleh.edu@gmail.com>
2023-03-24 13:24:12 +0100

    updates real time with database

--- src/components/profile/UserAddressCard.tsx
+++ src/components/profile/UserAddressCard.tsx
@@ -1,7 +1,9 @@
 import {Card, CardActions, CardContent, Typography} from "@mui/material";
 import ScrollDialog from "../../components/dialogs/ScrollDialog";
 import AddressForm from "../../components/forms/AddressForm";
-import React from "react";
+import React, {useEffect, useState} from "react";
+import axios from "axios";
+import keycloak from "../../keycloak";
 
 interface User {
     address: string;
@@ -17,6 +19,49 @@ interface UserDetailsCardProps {
 }
 
 export function UserAddressCard({ user, onSubmit }: UserDetailsCardProps) {
+    const [userObject, setUserObject] = useState({
+        address: "",
+        post_code: "",
+        city: "",
+        country: "",
+    });
+
+    const [isFormSubmitted, setIsFormSubmitted] = useState(false);
+
+    //create user adress object
+
+    //TODO: useeffect to populate user info
+    //https://database-mefit.herokuapp.com/addresses/addressByUserId/1
+    useEffect(() => {
+        const profile:  Record<string, any>  = JSON.parse(localStorage.getItem('profile') || '{}');
+
+        const fetchAdress = async () => {
+            console.log("Fetch adress from profile page:");
+
+            const response = await axios.get(`https://database-mefit.herokuapp.com/addresses/addressByUserId/${profile.user}`, {
+                headers: {
+                    'Authorization': `Bearer ${keycloak.token}`,
+                    'Content-Type': 'application/json',
+                }
+            })
+            setUserObject({  address: response.data.address,
+                post_code: response.data.post_code,
+                city: response.data.city,
+                country:response.data.country,})
+
+        }
+
+        fetchAdress();
+        console.log(userObject)
+    },[isFormSubmitted]);
+    const handleFormSubmit = () => {
+        if (isFormSubmitted == true){
+
+            setIsFormSubmitted(false);
+        }else {
+            setIsFormSubmitted(true);
+        }
+    };
 
 
 
@@ -28,13 +73,13 @@ export function UserAddressCard({ user, onSubmit }: UserDetailsCardProps) {
                         User details
                     </Typography>
                     <Typography variant="body2" sx={{ fontSize: 16 }}>
-                        Address: {user.address} <br />
-                        Postal code: {user.post_code} <br />
-                        City: {user.city} <br />
-                        Country: {user.country} <br />
+                        Address: {userObject.address} <br />
+                        Postal code: {userObject.post_code} <br />
+                        City: {userObject.city} <br />
+                        Country: {userObject.country} <br />
                     </Typography>
                     <CardActions>
-                        <ScrollDialog content={<AddressForm onSubmit={onSubmit} headerText={"Update info"} />} buttonText="Update" headerText="Update info" />
+                        <ScrollDialog content={<AddressForm onSubmit={handleFormSubmit} headerText={"Update info"} />} buttonText="Update" headerText="Update info" />
                     </CardActions>
                 </CardContent>
             </Card>
