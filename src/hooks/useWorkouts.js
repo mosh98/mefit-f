a3d735424c21c48304cae3a5f5414ae38924b030
LBohman <lovisabo@hotmail.com>
2023-03-24 10:56:01 +0100

    I tried so hard and got so far, but in the end it doesn't even matter

--- src/hooks/useWorkouts.js
+++ src/hooks/useWorkouts.js
@@ -1,5 +1,4 @@
 import { useEffect, useState } from "react";
-import { fetchExercises } from "../api/exercises";
 import { fetchWorkouts } from "../api/workouts";
 
 /**
@@ -17,23 +16,18 @@ function useWorkouts() {
       setWorkouts(workouts);
       setError(error);
 
-      const exerciseIdList = workouts.flatMap(workout => workout.exercises.flat());
-      const exerciseDetails = await Promise.all(exerciseIdList.map(id => fetchExercises(`exercises/exerciseById/${id}`)));
-      console.log(exerciseDetails)
-      const workoutsWithData = workouts.map(workout => ({
-        ...workout,
-        exercises: workout.exercises.map(id => exerciseDetails.exercises.find(exercise => exercise.id === id))
-      }));
-      console.log(workoutsWithData);
+      // Tried to map exercise ids to actual exercises with names and descriptions, haven't managed to do that... (ノಥ,_｣ಥ)ノ彡┻━┻
 
-      // const exerciseIdList = workouts.flatMap(workout => workout.exercises.flat());
-      // const exerciseDetails = await Promise.all(exerciseIdList.map(id => fetchWorkouts(`exercises/exerciseById/${id}`)));
-      // console.log(exerciseDetails);
+      // const exerciseIdList = workouts.flatMap(workout => workout.exercises).flat();
+      // console.log(exerciseIdList)
+      // const exerciseDetails = await Promise.all(exerciseIdList.map(id => fetchExercises(`exercises/exerciseById/${id}`)));
+      // console.log(exerciseDetails)
+
+    
       // const workoutsWithData = workouts.map(workout => ({
       //   ...workout,
-      //   exercises: workout.exercises.map(id => exerciseDetails.find(exercise => exercise.id === id))
+      //   // exercises: workout.exercises.map(id => exerciseDetails.exercises.find(exercise => exercise.id === id))
       // }));
-      // console.log(workoutsWithData);
     };
 
     init();
